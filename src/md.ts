2f38ec1ef76d79efc01fb129a7f40b7675b97334
pavlov.dan <pavlov.dan@panoramik.ru>
2021-10-12 19:57:11 +0500

    Moved to TS

---
+++ src/md.ts
@@ -0,0 +1,19 @@
+import { color } from './colors';
+export function markdown(text: string, options?: {bold?: boolean, italic?: boolean, mono?: boolean, link?: string}) {
+    let result = text;
+    if (options) {
+        if (options.bold) {
+            result = color(`**${result}**`, { font: 'yellow', effects: ['bright'] });
+        }
+        if (options.italic) {
+            result = color(`_${result}_`, { font: 'magenta', effects: ['italic'] });
+        }
+        if (options.mono) {
+            result = color(`\`${result}\``, { font: 'green' });
+        }
+        if (options.link) {
+            result = `[${result}](${color(options.link, { font: 'blue', effects: ['underscore'] })})`;
+        }
+    }
+    return result;
+}
