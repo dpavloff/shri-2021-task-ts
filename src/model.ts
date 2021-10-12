d968d7041e43849f55b9a28029c1cbe20c1395f0
pavlov.dan <pavlov.dan@panoramik.ru>
2021-10-12 19:19:43 +0500

    Finished style refactoring

--- src/model.ts
+++ src/model.ts
@@ -3,9 +3,19 @@ const Reset = '\x1b[0m';
 type Color = 'black' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white';
 
 type ColorOptions = {
-    font: Color,
-    background: Color,
-    effects: string[]
+    font: Color;
+    background: Color;
+    effects: string[];
+};
+
+const isColorOptions = (arg: any): arg is ColorOptions => {
+    if (
+        (arg as ColorOptions).font !== undefined ||
+        (arg as ColorOptions).effects !== undefined ||
+        (arg as ColorOptions).background !== undefined
+    )
+        return true;
+    else return false;
 }
 
 type MarkdownOptions = {
@@ -15,8 +25,17 @@ type MarkdownOptions = {
     link: string;
 };
 
+const isMarkdownOptions = (arg: any): arg is MarkdownOptions => {
+    if (
+        (arg as MarkdownOptions).bold !== undefined ||
+        (arg as MarkdownOptions).italic !== undefined ||
+        (arg as MarkdownOptions).mono !== undefined ||
+        (arg as MarkdownOptions).link !== undefined 
+    ) return true;
+    else return false;
+}
 
-const effects : Record<string, string> = {
+const effects: Record<string, string> = {
     bright: '\x1b[1m',
     dim: '\x1b[2m',
     italic: '\x1b[3m',
@@ -24,7 +43,7 @@ const effects : Record<string, string> = {
     blink: '\x1b[5m',
 };
 
-const fontColors : Record<Color, string> = {
+const fontColors: Record<Color, string> = {
     black: '\x1b[30m',
     red: '\x1b[31m',
     green: '\x1b[32m',
@@ -35,7 +54,7 @@ const fontColors : Record<Color, string> = {
     white: '\x1b[37m',
 };
 
-const backgroundColors : Record<Color, string> = {
+const backgroundColors: Record<Color, string> = {
     black: '\x1b[40m',
     red: '\x1b[41m',
     green: '\x1b[42m',
@@ -46,7 +65,7 @@ const backgroundColors : Record<Color, string> = {
     white: '\x1b[47m',
 };
 
-const contrast : Record<Color, string> = {
+const contrast: Record<Color, string> = {
     black: 'white',
     red: 'black',
     green: 'black',
@@ -57,4 +76,4 @@ const contrast : Record<Color, string> = {
     white: 'black',
 };
 
-export { Reset, effects, fontColors, backgroundColors, contrast, Color, ColorOptions, MarkdownOptions }
+export { Reset, effects, fontColors, backgroundColors, contrast, Color, ColorOptions, MarkdownOptions, isColorOptions, isMarkdownOptions };
--- src/style.ts
+++ src/style.ts
@@ -1,16 +1,16 @@
 import { color } from './colors';
 import { markdown } from './md';
 
-import { ColorOptions, MarkdownOptions } from './model';
+import { ColorOptions, MarkdownOptions, isColorOptions, isMarkdownOptions } from './model';
 
 export function style(text : string, options: Partial<ColorOptions> | Partial<MarkdownOptions>) {
     if (text.length === 0) {
         return text;
     }
-    if (options instanceof ColorOptions) {
+    if (isColorOptions(options)) {
         return color(text, options);
     }
-    if ('bold' in options || 'italic' in options || 'mono' in options || 'link' in options) {
+    if (isMarkdownOptions(options)) {
         return markdown(text, options);
     }
     return text;
